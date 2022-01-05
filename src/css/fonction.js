66f879b97069e470941f90332a8252904dcf1096
lavaine-iris <iris.lavaine@edu.univ-fcomte.fr>
2022-01-05 10:58:53 +0100

    Animations légende sur les images

--- src/css/fonction.js
+++ src/css/fonction.js
@@ -19,6 +19,114 @@ function descendreLegende() {
     }).play;
 }
 
+function monterLegende1() {
+    document.querySelector('.texte14').style.display = 'block';
+    return anime ({
+        targets: '.texte14',
+        translateY: '-4em',
+        autoplay: false,
+        duration: 300, 
+        easing: 'linear'
+    }).play;
+}
+
+function descendreLegende1() {
+    return anime ({
+        targets: '.texte14',
+        translateY: 0,
+        autoplay: false,
+        duration: 300, 
+        easing: 'linear'
+    }).play;
+}
+
+function monterLegende2() {
+    document.querySelector('.texte15').style.display = 'block';
+    return anime ({
+        targets: '.texte15',
+        translateY: '-4em',
+        autoplay: false,
+        duration: 300, 
+        easing: 'linear'
+    }).play;
+}
+
+function descendreLegende2() {
+    return anime ({
+        targets: '.texte15',
+        translateY: 0,
+        autoplay: false,
+        duration: 300, 
+        easing: 'linear'
+    }).play;
+}
+
+function monterLegende3() {
+    document.querySelector('.texte16').style.display = 'block';
+    return anime ({
+        targets: '.texte16',
+        translateY: '-4em',
+        autoplay: false,
+        duration: 300, 
+        easing: 'linear'
+    }).play;
+}
+
+function descendreLegende3() {
+    return anime ({
+        targets: '.texte16',
+        translateY: 0,
+        autoplay: false,
+        duration: 300, 
+        easing: 'linear'
+    }).play;
+}
+
+function monterLegende4() {
+    document.querySelector('.texte17').style.display = 'block';
+    return anime ({
+        targets: '.texte17',
+        translateY: '-4em',
+        autoplay: false,
+        duration: 300, 
+        easing: 'linear'
+    }).play;
+}
+
+function descendreLegende4() {
+    return anime ({
+        targets: '.texte17',
+        translateY: 0,
+        autoplay: false,
+        duration: 300, 
+        easing: 'linear'
+    }).play;
+}
+
+function monterLegende5() {
+    document.querySelector('.texte18').style.display = 'block';
+    return anime ({
+        targets: '.texte18',
+        translateY: '-4em',
+        autoplay: false,
+        duration: 300, 
+        easing: 'linear'
+    }).play;
+}
+
+function descendreLegende5() {
+    return anime ({
+        targets: '.texte18',
+        translateY: 0,
+        autoplay: false,
+        duration: 300, 
+        easing: 'linear'
+    }).play;
+}
+
+
+
+
 function carrousel() {
 
     let images = document.querySelectorAll('.image15');
